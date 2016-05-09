cafb961952628251a856bd36c76bca8e1c4c5f2b
Andrew Lee <email": null>
2016-05-09 14:43:37 -0700

    complete SodaMachine class and pass rspec

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,19 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    sodas.select { |soda| soda.brand == soda_brand } [0]
   end
 
   def sell(soda_brand)
+    selected_soda = sodas.select { |soda| soda.brand == soda_brand } [0]
+    unless selected_soda.nil?
+      @cash += selected_soda.price
+    end
+    sodas.delete(selected_soda)
   end
 
 end
